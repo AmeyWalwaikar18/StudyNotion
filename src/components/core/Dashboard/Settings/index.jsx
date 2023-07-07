939595989c7a5856fa56157610b24c57fe5a40ae
AmeyWalwaikar18 <walwaikaramey18@gmail.com>
2023-07-08 02:22:50 +0530

    margin increased

--- src/components/core/Dashboard/Settings/index.jsx
+++ src/components/core/Dashboard/Settings/index.jsx
@@ -6,7 +6,7 @@ import UpdatePassword from "./UpdatePassword"
 export default function Settings() {
     return (
         <div>
-            <h1 className="mb-14 text-3xl font-medium text-richblack-5 ml-4 md:ml-0">Edit Profile</h1>
+            <h1 className="mb-14 text-3xl font-medium text-richblack-5 ml-16 md:ml-0">Edit Profile</h1>
             {/* change profile picture */}
             <ChangeProfilePicture/>
             {/* edit profile */}
