b423163e9f9c1ce7d4c663c9131cf6a8cdf85dc5
AmeyWalwaikar18 <walwaikaramey18@gmail.com>
2023-07-10 13:38:26 +0530

    minor changes

--- Server/controllers/ContactUs.js
+++ Server/controllers/ContactUs.js
@@ -2,7 +2,7 @@ const {contactUsEmail} = require("../mail/templates/ContactFormRes");
 const mailSender = require("../utils/mailSender")
 
 exports.contactUsController = async (req, res) => {
-  const { email, firstname, lastname, message, phoneNo, countrycode } = req.body
+  const { email, firstname, lastname, message, phoneNo, countrycode } = req.body;
   console.log(req.body)
   try {
     const emailRes = await mailSender(
@@ -11,14 +11,14 @@ exports.contactUsController = async (req, res) => {
       contactUsEmail(email, firstname, lastname, message, phoneNo, countrycode)
     )
     console.log("Email Res ", emailRes)
-    return res.json({
+    return res.status(200).json({
       success: true,
       message: "Email send successfully",
     })
   } catch (error) {
     console.log("Error", error)
     console.log("Error message :", error.message)
-    return res.json({
+    return res.status(500).json({
       success: false,
       message: "Something went wrong...",
     })
--- Server/controllers/Profile.js
+++ Server/controllers/Profile.js
@@ -22,7 +22,7 @@ exports.updateProfile = async (req, res) => {
 		// Save the updated profile
 		await profile.save();
 
-		return res.json({
+		return res.status(200).json({
 			success: true,
 			message: "Profile updated successfully",
 			profile,
